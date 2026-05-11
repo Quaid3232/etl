30f5be1e57aaa8826d641afc962a14532c46ff3c
Quaid Khan <quaidali@gmail.com>
2026-05-11 15:28:15 +0500

    add projrct base structure

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+
+-- create table
+CREATE TABLE OR REPALCE employee (
+    employeeID int primary key, unique,
+    name varchar(250),
+    age int,
+    department varchar(250),
+    salary int,
+    joindate date
+)
