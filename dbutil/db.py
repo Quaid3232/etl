c5fa176d5436faf158a141a72dd1f449e40cb63e
Quaid Khan <quaidali@gmail.com>
2026-05-13 19:43:42 +0500

    add path for source

---
+++ dbutil/db.py
@@ -0,0 +1,4 @@
+# path = r'C:\Users\intel computer\Desktop\employee.csv'
+
+source_path = r'C:\Users\intel computer\Documents\Learning Git\data\raw\employee.csv'
+badRecordPath = r'C:\Users\intel computer\Documents\Learning Git\data\quarantine'
