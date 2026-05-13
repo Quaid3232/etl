a9a30a3d7f0db7c206411627db70f236f2b4c7bc
Quaid Khan <quaidali@gmail.com>
2026-05-13 20:06:20 +0500

    add proper logging in extraction file

--- script/extract.py
+++ script/extract.py
@@ -3,16 +3,21 @@ from loguru import logger
 
 def extraction(path):
     df = None
+    logger.info("data extraction starting...")
     try:
         if path.endswith(".json"):
             df=pd.read_json(path)
+            logger.info(f'file format is json')
         elif path.endswith('.csv'):
             df = pd.read_csv(path)
+            logger.info(f'file format is csv')
         elif path.endswith('.parquet'):
             df = pd.read_parquet(path)
+            logger.info(f'file format is parquet')
         else:
             logger.error(f"file format is inviled: {path}")
             return None
     except Exception as e:
         logger.error(f'error: {e}')
     return df
+    logger.info("data extraction finished...")
